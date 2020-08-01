88d8b0fef903c9b537507733c8e85689b4045482
Lucas <zhitongdaohe3@gmail.com>
2020-08-01 14:51:09 +0800

    Update svhn2mnist.py

--- digit_signal_classification/model/svhn2mnist.py
+++ digit_signal_classification/model/svhn2mnist.py
@@ -29,8 +29,7 @@ class Feature(nn.Module):
 
 class Predictor(nn.Module):
     def __init__(
-            self, 
-            num_classifiers_train=2,
+            self, num_classifiers_train=2,
             num_classifiers_test=20,
             init='kaiming_u',
             use_init=False
@@ -46,70 +45,58 @@ class Predictor(nn.Module):
             'xavier': nn.init.xavier_normal_
         }
 
-        self.mu1 = Parameter(torch.randn(3072, 2048))
-        self.sigma1 = Parameter(torch.zeros(3072, 2048))
+        self.fc1 = nn.Linear(3072, 2048)
 
-        self.mu2 = Parameter(torch.randn(2048, 10))
-        self.sigma2 = Parameter(torch.zeros(2048, 10))
+        self.mu2 = Parameter(torch.randn(10, 2048))
+        self.sigma2 = Parameter(torch.zeros(10, 2048))
 
         if use_init:
-            all_parameters = [self.mu1, self.sigma1, self.mu2, self.sigma2]
+            all_parameters = [self.mu2, self.sigma2]
             for item in all_parameters:
                 function_init[self.init](item)
 
-        self.b1 = Parameter(torch.zeros(2048))
         self.b2 = Parameter(torch.zeros(10))
         self.bn1_fc = nn.BatchNorm1d(2048)
 
     def forward(self, x, only_mu=True):
-        # Keep sigma values be positive
-        sigma1_pos = torch.sigmoid(self.sigma1)
-        sigma2_pos = torch.sigmoid(self.sigma2)
 
-        fc1_distribution = normal.Normal(self.mu1, sigma1_pos)
+        x = self.fc1(x)
+        x = F.relu(self.bn1_fc(x))
+
+        sigma2_pos = torch.sigmoid(self.sigma2)
         fc2_distribution = normal.Normal(self.mu2, sigma2_pos)
 
         if self.training:
             classifiers = []
             for index in range(self.num_classifiers_train):
-                fc1_w = fc1_distribution.rsample()
                 fc2_w = fc2_distribution.rsample()
-                one_classifier = [fc1_w, self.b1, fc2_w, self.b2]
+                one_classifier = [fc2_w, self.b2]
 
                 classifiers.append(one_classifier)
 
             outputs = []
             for index in range(self.num_classifiers_train):
-                out = torch.matmul(x, classifiers[index][0]) + classifiers[index][1]
-                out = F.relu(self.bn1_fc(out))
-                out = torch.matmul(out, classifiers[index][2]) + classifiers[index][3]
+                out = F.linear(x,  classifiers[index][0], classifiers[index][1])
                 outputs.append(out)
 
             return outputs
         else:
             if only_mu:
                 # Only use mu for classification
-                mus = [self.mu1, self.mu2, self.b1, self.b2]
-
-                out = torch.matmul(x, mus[0]) + mus[2]
-                out = F.relu(self.bn1_fc(out))
-                out = torch.matmul(out, mus[1]) + mus[3]
+                out = F.linear(x, self.mu2, self.b2)
 
                 return [out]
             else:
                 classifiers = []
                 for index in range(self.num_classifiers_test):
-                    fc1_w = fc1_distribution.rsample()
                     fc2_w = fc2_distribution.rsample()
-                    one_classifier = [fc1_w, self.b1, fc2_w, self.b2]
+                    one_classifier = [fc2_w, self.b2]
 
                     classifiers.append(one_classifier)
 
                 outputs = []
                 for index in range(self.num_classifiers_test):
-                    out = torch.matmul(x, classifiers[index][0]) + classifiers[index][1]
-                    out = F.relu(self.bn1_fc(out))
-                    out = torch.matmul(out, classifiers[index][2]) + classifiers[index][3]
+                    out = F.linear(x, classifiers[index][0], classifiers[index][1])
                     outputs.append(out)
 
                 return outputs
